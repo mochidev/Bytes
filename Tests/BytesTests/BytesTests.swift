4387d18bcc81bbfc7f45099d69fa939b73f7be2a
Dimitri Bouniol <dimitribouniol@mochidev.com>
2020-11-07 01:21:44 -0800

    Added Type test

--- Tests/BytesTests/BytesTests.swift
+++ Tests/BytesTests/BytesTests.swift
@@ -2,7 +2,15 @@ import XCTest
 @testable import Bytes
 
 final class BytesTests: XCTestCase {
+    func testTypes() throws {
+        XCTAssertTrue(Bytes.self == Array<UInt8>.self)
+        XCTAssertTrue(BytesSlice.self == ArraySlice<UInt8>.self)
+        XCTAssertTrue(ContiguousBytes.self == ContiguousArray<UInt8>.self)
+        
+        XCTAssertFalse(Bytes.self == Array<Int8>.self)
+    }
 
     static var allTests = [
+        ("testTypes", testTypes),
     ]
 }

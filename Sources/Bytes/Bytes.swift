3f4af0ebd5625be84c084f2c887aa49a1ca69c61
Dimitri Bouniol <dimitribouniol@mochidev.com>
2020-11-07 00:29:14 -0800

    Added Bytes, BytesSlice, ContiguousBytes, and BytesCollection types

--- Sources/Bytes/Bytes.swift
+++ Sources/Bytes/Bytes.swift
@@ -6,3 +6,14 @@
 //  Copyright © 2020 Mochi Development, Inc. All rights reserved.
 //
 
+/// A type that has the same API as `Bytes`, `BytesSlice`, and `ContiguousBytes`.
+public protocol BytesCollection: CustomDebugStringConvertible, CustomReflectable, CustomStringConvertible, ExpressibleByArrayLiteral, MutableCollection, RandomAccessCollection, RangeReplaceableCollection where Element == UInt8, Index == Int, SubSequence: BytesCollection {}
+
+public typealias Bytes = Array<UInt8>
+extension Bytes: BytesCollection {}
+
+public typealias BytesSlice = ArraySlice<UInt8>
+extension BytesSlice: BytesCollection {}
+
+public typealias ContiguousBytes = ContiguousArray<UInt8>
+extension ContiguousBytes: BytesCollection {}

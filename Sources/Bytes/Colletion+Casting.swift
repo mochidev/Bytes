d9c2f58c9d6dc428034dd64e648a364a38aa1d5f
Dimitri Bouniol <dimitribouniol@mochidev.com>
2020-11-08 13:16:34 -0800

    Added documentation to existing casting methods

--- Sources/Bytes/Colletion+Casting.swift
+++ Sources/Bytes/Colletion+Casting.swift
@@ -9,11 +9,22 @@
 import Foundation
 
 extension BidirectionalCollection where Element == UInt8 {
+    /// Cast an _entire_ Bytes sequence to the target's type.
+    /// - Parameter target: The type of the target.
+    /// - Throws:
+    ///     - `BytesError.invalidMemorySize` if the memory layout of the bytes sequence does not match the desired type.
+    ///     - `BytesError.contiguousMemoryUnavailable` if contiguous memory could not be made available.
+    /// - Returns: An instance of the target represented by the Bytes sequence.
     @inlinable
     public func casting<R>(to target: R.Type) throws -> R {
         try casting()
     }
     
+    /// Cast an _entire_ Bytes sequence to the lhs's type.
+    /// - Throws:
+    ///     - `BytesError.invalidMemorySize` if the memory layout of the bytes sequence does not match the desired type.
+    ///     - `BytesError.contiguousMemoryUnavailable` if contiguous memory could not be made available.
+    /// - Returns: An instance represented by the Bytes sequence.
     @inlinable
     public func casting<R>() throws -> R {
         guard MemoryLayout<R>.size == self.count else {
@@ -32,6 +43,8 @@ extension BidirectionalCollection where Element == UInt8 {
         return result
     }
     
+    /// Create a new Bytes sequence from the memory occupied by the passed in value.
+    /// - Parameter value: The value to cast to a sequence of bytes.
     @inlinable
     public init<T>(casting value: T) where Self: RangeReplaceableCollection {
         self = withUnsafeBytes(of: value) { Self($0) }

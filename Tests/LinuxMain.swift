cd99c369b9289c109537ec578f76a56a3e643a40
Dimitri Bouniol <dimitribouniol@mochidev.com>
2020-11-07 21:07:14 -0800

    Added headers to test sources

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,3 +1,11 @@
+//
+//  LinuxMain.swift
+//  Bytes
+//
+//  Created by Dimitri Bouniol on 11/6/20.
+//  Copyright © 2020 Mochi Development, Inc. All rights reserved.
+//
+
 import XCTest
 
 import BytesTests

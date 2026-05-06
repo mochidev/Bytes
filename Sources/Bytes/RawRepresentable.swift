873b44c15a509a55aa7d9b74ec4b3d62f909c791
Dimitri Bouniol <dimitribouniol@mochidev.com>
2026-05-06 03:38:24 -0700

    Updated copyright headers on all pages

--- Sources/Bytes/RawRepresentable.swift
+++ Sources/Bytes/RawRepresentable.swift
@@ -1,9 +1,10 @@
 //
 //  RawRepresentable.swift
-//  Bytes
+//  https://github.com/mochidev/Bytes
 //
 //  Created by Dimitri Bouniol on 11/7/20.
-//  Copyright © 2020 Mochi Development, Inc. All rights reserved.
+//  Copyright © 2020-26 Mochi Development, Inc. All rights reserved.
+//  mochidev-swift-bytes: F44D5591194F47C0834EC1EBD0102932
 //
 
 extension RawRepresentable {
